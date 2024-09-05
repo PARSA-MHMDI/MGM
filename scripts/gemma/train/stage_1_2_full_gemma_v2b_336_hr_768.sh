d18a41da8e0c41ac92bc38f39135f48e14ce1792
Parsa Mohammadi <mhmdi.prs@gmail.com>
2024-09-05 23:47:29 +0330

    Update stage_1_2_full_gemma_v2b_336_hr_768.sh

--- scripts/gemma/train/stage_1_2_full_gemma_v2b_336_hr_768.sh
+++ scripts/gemma/train/stage_1_2_full_gemma_v2b_336_hr_768.sh
@@ -3,12 +3,17 @@ PRETRAIN_NAME=MGM-2B-Pretrain
 FINETUNE_NAME=MGM-2B
 AUX_SIZE=768
 
+#!/bin/bash
+PRETRAIN_NAME=MGM-2B-Pretrain
+FINETUNE_NAME=Parsa
+AUX_SIZE=768
+
 deepspeed mgm/train/train_mem.py \
-    --deepspeed ./scripts/zero2_offload.json \
-    --model_name_or_path model_zoo/LLM/gemma/gemma-2b-it \
+    --deepspeed ./scripts/zero2.json \
+    --model_name_or_path /content/MGM/work_dirs/MGM/MGM-2B \
     --version gemma \
-    --data_path ./data/MGM-Pretrain/mgm_pretrain.json \
-    --image_folder ./data/MGM-Pretrain \
+    --data_path /content/MGM/dataset.json \
+    --image_folder /content/MGM/content/content/combined/CXR_png \
     --vision_tower model_zoo/OpenAI/clip-vit-large-patch14-336 \
     --vision_tower_aux model_zoo/OpenAI/openclip-convnext-large-d-320-laion2B-s29B-b131K-ft-soup \
     --mm_projector_type mlp2x_gelu \
@@ -16,52 +21,14 @@ deepspeed mgm/train/train_mem.py \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end False \
     --mm_use_im_patch_token False \
-    --image_size_aux $AUX_SIZE \
-    --bf16 True \
-    --output_dir ./work_dirs/$PRETRAIN_NAME \
-    --num_train_epochs 1 \
-    --per_device_train_batch_size 8 \
-    --per_device_eval_batch_size 4 \
-    --gradient_accumulation_steps 4 \
-    --evaluation_strategy "no" \
-    --save_strategy "steps" \
-    --save_steps 24000 \
-    --save_total_limit 1 \
-    --learning_rate 1e-3 \
-    --weight_decay 0. \
-    --warmup_ratio 0.03 \
-    --lr_scheduler_type "cosine" \
-    --logging_steps 1 \
-    --tf32 True \
-    --model_max_length 2048 \
-    --gradient_checkpointing True \
-    --dataloader_num_workers 4 \
-    --lazy_preprocess True \
-    --report_to wandb
-
-
-deepspeed mgm/train/train_mem.py \
-    --deepspeed ./scripts/zero2.json \
-    --model_name_or_path model_zoo/LLM/gemma/gemma-2b-it \
-    --version gemma \
-    --data_path ./data/MGM-Finetune/mgm_instruction.json \
-    --image_folder ./data/MGM-Finetune \
-    --vision_tower model_zoo/OpenAI/clip-vit-large-patch14-336 \
-    --vision_tower_aux model_zoo/OpenAI/openclip-convnext-large-d-320-laion2B-s29B-b131K-ft-soup \
-    --pretrain_mm_mlp_adapter ./work_dirs/$PRETRAIN_NAME/mm_projector.bin \
-    --mm_projector_type mlp2x_gelu \
-    --mm_vision_select_layer -2 \
-    --mm_use_im_start_end False \
-    --mm_use_im_patch_token False \
     --image_aspect_ratio pad \
-    --group_by_modality_length True \
     --image_size_aux $AUX_SIZE \
     --bf16 True \
     --output_dir ./work_dirs/$FINETUNE_NAME \
-    --num_train_epochs 1 \
-    --per_device_train_batch_size 8 \
-    --per_device_eval_batch_size 4 \
-    --gradient_accumulation_steps 2 \
+    --num_train_epochs 10 \
+    --per_device_train_batch_size 1 \
+    --per_device_eval_batch_size 1 \
+    --gradient_accumulation_steps 1 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
     --save_steps 1000 \
@@ -72,8 +39,8 @@ deepspeed mgm/train/train_mem.py \
     --lr_scheduler_type "cosine" \
     --logging_steps 1 \
     --tf32 True \
-    --model_max_length 2048 \
+    --model_max_length 2024 \
     --gradient_checkpointing True \
     --dataloader_num_workers 4 \
     --lazy_preprocess True \
-    --report_to wandb
+    --report_to tensorboard
